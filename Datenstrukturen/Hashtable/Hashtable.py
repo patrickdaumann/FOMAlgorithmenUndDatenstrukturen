925b32bec3fd12f18fc5674279253cda6e08ddfc
Patrick Daumann <59425260+patrickdaumann@users.noreply.github.com>
2023-01-25 21:25:31 +0100

    Working Version of Chained Hashtable

--- Datenstrukturen/Hashtable/Hashtable.py
+++ Datenstrukturen/Hashtable/Hashtable.py
@@ -1,71 +1,55 @@
-class linkedlist:
-    def __init__(self) -> None:
-        self.head = None
-    
-    def addnode(self, newnode) -> None:
-        if self.head == None:
-            self.head = newnode
+class HashTable(object):
+    def __init__(self, size):
+        self.values = [None] * size
+
+    def hashfunction(self, x: int):
+        return x % len(self.values)
+
+    def insert(self, value: any):
+        index = self.hashfunction(value)
+        if self.values[index] == None:
+            self.values[index] = LinkedList()
         
-        else:
-            x = self.head
-            while x.next != None:
-                x = x.next
+        self.values[index].listInsert(value)
 
-            x.next = newnode
+    def PrintValues(self):
+        for i in self.values:
+            i.listPrint()
 
-    def printlist(self) -> None:
-        x = self.head
-        while x != None:
-            x.dataval.printvalues()
-            x = x.next
-
-
-class listnode:
-    def __init__(self, dataval) -> None:
+class ListNode(object):
+    def __init__(self, dataval:any):
         self.dataval = dataval
         self.next = None
 
-class Hashtable:
-    def __init__(self, size: int) -> None:
-        self.values = [None] * size
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
 
-    def add(self, value: any, key: any, hashfunction):
-
-        address = hashfunction(key, len(self.values))
-        if self.values[address] == None:
-            self.values[address] = linkedlist()
-
+    def listInsert(self, Value: any):
+        NewNode = ListNode(Value)
+        if self.head == None:
+            self.head = NewNode
         else:
-            self.values[address].addnode(listnode(value))
+            x = self.head
+            self.head = NewNode
+            self.head.next = x
+    
+    def listPrint(self):
+        x = self.head
+        if(x != None):
+            print(x.dataval, end="")
+        while x.next != None:
+            x = x.next
+            print(f" - {x.dataval}", end = "")
 
-    def findvalue(self, key: any, hashfunction):
-        return self.values[hashfunction(key, len(self.values))]
+        print()
+
+###############################
+# Testcode
+H1 = HashTable(10)
+
+for i in range(100):
+    H1.insert(i)
 
 
-class user:
-    def __init__(self, username: str, id: int, firstname: str, surname: str, email: str) -> None:
-        self.username = username
-        self.id = id
-        self.firstname = firstname
-        self.surname = surname
-        self.email = email
-
-    def printvalues(self) -> None:
-        print(f"Username: {self.username}\nID: {self.id}\nFirstname: {self.firstname}\nSurname: {self.surname}\nE-Mail: {self.email}")
-
-
-
-########################################
-hashfunction = lambda x, n:x % n
-
-user1 = user("JDoe19", 999, "John", "Doe", "john.doe@contoso.com")
-
-Hash1 = Hashtable(size=20)
-
-Hash1.add(user1, user1.id, hashfunction)
-
-Hash1.findvalue(user1.id, hashfunction).printlist()
-
-for x in Hash1.values:
-    if x is Hashtable:
-        x.printlist()
+H1.PrintValues()
